739d5279d654299a6dbcf9174d305fa40c06c6b3
Praneeth2kp <praneeth2kp@gmail.com>
2023-10-12 16:43:30 +0530

    ..dice1

--- components/MyDiceRoll.tsx
+++ components/MyDiceRoll.tsx
@@ -24,42 +24,61 @@ const options = {
 };
 
 const MyDiceRoll = () => {
+
+  const InverseTxtColor = (bgcolor: String) => {
+    const r = parseInt(bgcolor.slice(1, 3), 16);
+    const g = parseInt(bgcolor.slice(3, 5), 16);
+    const b = parseInt(bgcolor.slice(5, 7), 16);
+    const Brightness = (r * 299 + g * 587 + b * 114) / 1000;
+    return Brightness >= 128 ? '#000000' : '#FFFFFF';
+  };
   const [DiceImage, setDiceImage] = useState(Dice1);
   const [DiceImage1, setDiceImage1] = useState(Dice2);
-  const [RandomColor, setRandomColor] = useState('#000000');
+  const [RandomBGColor, setRandomBGColor] = useState('#000000');
+  const [RandomTextColor, setRandomTextColor] = useState(
+    InverseTxtColor(RandomBGColor),
+  );
   const Dice = [Dice1, Dice2, Dice3, Dice4, Dice5, Dice6];
 
   const ImageSelection = () => {
     const randomIndex = Math.floor(Math.random() * 6);
     return Dice[randomIndex];
   };
+
   const RandomBG = () => {
-    const RandomColor = '0123456789ABCDEF';
+    const RandomBGColor = '0123456789ABCDEF';
     let color = '#';
     for (let i = 0; i < 6; ++i) {
-      color += RandomColor[Math.floor(Math.random() * 16)];
+      color += RandomBGColor[Math.floor(Math.random() * 16)];
     }
     return color;
   };
   const RollDice = () => {
     const newImage = ImageSelection();
     const newImage1 = ImageSelection();
+    const newBG = RandomBG();
+    const newTxt = InverseTxtColor(newBG);
     setDiceImage(newImage);
     setDiceImage1(newImage1);
-    setRandomColor(RandomBG());
+    setRandomBGColor(RandomBG());
+    setRandomTextColor(newTxt);
     ReactNativeHapticFeedback.trigger('impactLight', options);
   };
   return (
     <>
-      <View style={[styles.container, {backgroundColor: RandomColor}]}>
-        <Text style={styles.Title}>My Dice Roll</Text>
+      <View style={[styles.container, {backgroundColor: RandomBGColor}]}>
+        <Text style={[styles.Title, {color: RandomTextColor}]}>
+          My Dice Roll
+        </Text>
         <View style={styles.imageContainer}>
           <Image source={DiceImage} style={styles.Images} />
           <Image source={DiceImage1} style={styles.Images1} />
         </View>
         <TouchableOpacity onPress={RollDice}>
-          <View style={styles.Button}>
-            <Text style={styles.BtnTxt}>Roll The Dice</Text>
+          <View style={[styles.Button, {borderColor: RandomTextColor}]}>
+            <Text style={[styles.BtnTxt, {color: RandomTextColor}]}>
+              Roll The Dice
+            </Text>
           </View>
         </TouchableOpacity>
       </View>
@@ -72,7 +91,6 @@ export default MyDiceRoll;
 const styles = StyleSheet.create({
   Title: {
     fontSize: 30,
-    color: '#fff',
     marginBottom: 30,
   },
   container: {
@@ -97,13 +115,11 @@ const styles = StyleSheet.create({
     width: 200,
     borderRadius: 20,
     borderWidth: 2,
-    borderColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
     margin: 20,
   },
   BtnTxt: {
     fontSize: 24,
-    color: '#ffffff',
   },
 });
